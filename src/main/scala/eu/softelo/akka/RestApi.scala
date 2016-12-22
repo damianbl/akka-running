65ae9b404454d02c6cac3a042d9167ce6500e93e
damian <damian@withlocals.com>
2016-12-22 21:48:07 +0100

    finish routes

--- src/main/scala/eu/softelo/akka/RestApi.scala
+++ src/main/scala/eu/softelo/akka/RestApi.scala
@@ -10,7 +10,8 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
-import eu.softelo.akka.BoxOffice.{Events, GetEvents}
+import eu.softelo.akka.BoxOffice._
+import eu.softelo.akka.TicketSeller.Tickets
 
 import scala.concurrent.ExecutionContextExecutor
 
@@ -33,8 +34,51 @@ trait RestRoutes extends BoxOfficeApi with EventMarshalling {
       pathEndOrSingleSlash {
         get {
           // GET /events
-          onSuccess(getEvents()) { events =>
+          onSuccess(getEvents) { events =>
             complete(OK, events)
+          }
+        }
+      }
+    }
+
+  def eventRoute: Route =
+    pathPrefix("events" / Segment) { event =>
+      pathEndOrSingleSlash {
+        post {
+          // POST /events/:event
+          entity(as[EventDescription]) { ed =>
+            onSuccess(createEvent(event, ed.tickets)) {
+              case BoxOffice.EventCreated(eventCreated) => complete(Created, eventCreated)
+              case BoxOffice.EventExists =>
+                complete(BadRequest, Error(s"$event event exists already."))
+            }
+          }
+        } ~
+          get {
+            // GET /events/:event
+            onSuccess(getEvent(event)) {
+              _.fold(complete(NotFound))(e => complete(OK, e))
+            }
+          } ~
+          delete {
+            // DELETE /events/:event
+            onSuccess(cancelEvent(event)) {
+              _.fold(complete(NotFound))(e => complete(OK, e))
+            }
+          }
+      }
+    }
+
+  def ticketsRoute: Route =
+    pathPrefix("events" / Segment / "tickets") { event =>
+      post {
+        pathEndOrSingleSlash {
+          // POST /events/:event/tickets
+          entity(as[TicketRequest]) { request =>
+            onSuccess(requestTickets(event, request.tickets)) { tickets =>
+              if (tickets.entries.isEmpty) complete(NotFound)
+              else complete(Created, tickets)
+            }
           }
         }
       }
@@ -50,5 +94,15 @@ trait BoxOfficeApi {
 
   lazy val boxOffice: ActorRef = createBoxOffice()
 
-  def getEvents(): Future[Events] = boxOffice.ask(GetEvents).mapTo[Events]
+  def getEvents: Future[Events] = boxOffice.ask(GetEvents).mapTo[Events]
+
+  def getEvent(event: String): Future[Option[Event]] = boxOffice.ask(GetEvent(event)).mapTo[Option[Event]]
+
+  def createEvent(event: String, tickets: Int): Future[EventResponse] =
+    boxOffice.ask(CreateEvent(event, tickets)).mapTo[EventResponse]
+
+  def cancelEvent(event: String): Future[Option[Event]] = boxOffice.ask(CancelEvent).mapTo[Option[Event]]
+
+  def requestTickets(event: String, tickets: Int): Future[Tickets] =
+    boxOffice.ask(GetTickets(event, tickets)).mapTo[TicketSeller.Tickets]
 }
